02a65519cbda2eca2f5fb55a19c322d63974d883
Lisandro <98889906+xxLich@users.noreply.github.com>
2023-06-08 18:27:22 -0300

    Add files via upload
    Dados del 1 al 10 Parte1.(Cuadrados)

---
+++ DadosLiso/dados.h
@@ -0,0 +1,10 @@
+#ifndef DADOS_H_INCLUDED
+#define DADOS_H_INCLUDED
+
+void dibujarCuadrado(int , int );
+void dibujarDado(int , int , int );
+void dibujarPuntos(int ,int ,int );
+void dibujarBordes(int ,int );
+
+
+#endif // DADOS_H_INCLUDED
