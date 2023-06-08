5cafa253c1281e53cf279b1fa4f9d8d112724972
Weldeo <ivan.agustin.leany@gmail.com>
2023-06-08 13:55:49 -0300

    uwu
    a laburar

---
+++ Parcial-LAB-1/Menu.h
@@ -0,0 +1,28 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+#include <iostream>
+#include "rlutil.h"
+#include "Menu.h"
+#include "dado.h"
+
+
+void drawMenu(int);
+
+
+int selectOpciones(int & , int, int &);
+
+
+
+
+/// funcion principal
+/// Corre el juego
+
+void dosJugadores();
+
+
+
+
+
+
+#endif // MENU_H_INCLUDED
---
+++ Parcial-LAB-1/dado.h
@@ -0,0 +1,23 @@
+#ifndef DADO_H_INCLUDED
+#define DADO_H_INCLUDED
+
+#include<iostream>
+#include "rlutil.h"
+#include "Menu.h"
+#include "dado.h"
+
+
+
+void drawDado(int);
+
+void llenarVector(int [], int);
+
+void drawSixDados(int []);
+
+
+
+
+
+
+
+#endif // DADO_H_INCLUDED
