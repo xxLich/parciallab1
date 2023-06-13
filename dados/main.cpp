f3e90593751af27abac66a32ec0b446fa243ba91
Lisandro <98889906+xxLich@users.noreply.github.com>
2023-06-13 09:27:26 -0300

    Dados del 1 al 10
    No hare los romanos, mucho tiempo y tengo que avanzar en otras cosas, si hay tiempo, hare los romanos.

---
+++ dados/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "rlutil.h"
+#include "dados.h"
+using namespace std;
+
+int main(){
+
+dibujarDados(1,5,5);
+
+dibujarDados(2,15,5);
+
+dibujarDados(3,25,5);
+
+dibujarDados(4,35,5);
+
+dibujarDados(5,45,5);
+
+dibujarDados(6,55,5);
+
+dibujarDados(7,5,10);
+dibujarDados(8,15,10);
+dibujarDados(9,25,10);
+dibujarDados(10,35,10);
+
+rlutil::locate(1,24);
+
+return 0;
+}
