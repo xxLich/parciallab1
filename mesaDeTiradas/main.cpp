27126c073c05a1055eac3c857687cd10dad91ec0
Lisandro <98889906+xxLich@users.noreply.github.com>
2023-06-17 19:33:31 -0300

    Mesa para dados (Mejorar color)

---
+++ mesaDeTiradas/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "rlutil.h"
+#include "mesa.h"
+using namespace std;
+
+int main(){
+
+
+dibujarMesa(20,5);
+
+rlutil::locate(20,3);
+cout<<"Nombre de jugador: Juan Carlos";
+
+rlutil::locate(1,27);
+
+
+
+
+
+
+
+return 0;
+}
---
+++ mesaDeTiradas/mesa.h
@@ -0,0 +1,7 @@
+#ifndef MESA_H_INCLUDED
+#define MESA_H_INCLUDED
+void dibujarMesa(int ,int );
+void fondoMesa(int , int );
+void bordesMesa(int , int );
+
+#endif // MESA_H_INCLUDED
