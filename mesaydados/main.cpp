f441a4767fb329a2de9de3cde81f4f40f44dcac2
Lisandro <98889906+xxLich@users.noreply.github.com>
2023-06-21 22:42:02 -0300

    Add files via upload

---
+++ mesaydados/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "mesaydados.h"
+#include "rlutil.h"
+#include "dados.h"
+using namespace std;
+
+int main(){
+
+
+int dado=6;
+dado10Caras(30,5,dado);
+//dosDadosSeisCaras(30,5,dado);
+//tresDadosSeisCaras(30,5,dado);
+//cincoDadosSeisCaras(30,5,dado);
+rlutil::locate(10,28);
+return 0;
+}
