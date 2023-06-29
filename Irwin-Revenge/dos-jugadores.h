3386a9a83e47e6bbd57e081f501d55503edcfebc
Weldeo <105435939+Weldeo@users.noreply.github.com>
2023-06-29 19:36:43 -0300

    Add files via upload

---
+++ Irwin-Revenge/dos-jugadores.h
@@ -0,0 +1,23 @@
+#ifndef DOS-JUGADORES_H_INCLUDED
+#define DOS-JUGADORES_H_INCLUDED
+
+#include<iostream>
+#include "rlutil.h"
+#include "Menu.h"
+#include "dado.h"
+#include "dos-jugadores.h"
+#include <string>
+
+
+
+
+
+
+void pedirDatos(string[], int);
+
+
+
+
+
+
+#endif // DOS-JUGADORES_H_INCLUDED
