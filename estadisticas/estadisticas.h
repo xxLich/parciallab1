f777927741808bb642c261c268dce8aa37b04c15
Lisandro <98889906+xxLich@users.noreply.github.com>
2023-06-30 02:53:20 -0300

    Estadisticas
    Solo hay que llamar a los vectores de los puntos.

---
+++ estadisticas/estadisticas.h
@@ -0,0 +1,9 @@
+#ifndef ESTADISTICAS_H_INCLUDED
+#define ESTADISTICAS_H_INCLUDED
+void estadisticas(int,int);
+void nombreJuego(int ,int);
+void jugadores(int,int);
+
+
+
+#endif // ESTADISTICAS_H_INCLUDED
