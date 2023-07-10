8da0aa111e09cff7cca6339fce1de6c78baaf6c5
Weldeo <105435939+Weldeo@users.noreply.github.com>
2023-07-10 18:46:39 -0300

    Delete mesaydados directory

--- mesaydados/dados.h
+++
@@ -1,8 +0,0 @@
-#ifndef DADOS_H_INCLUDED
-#define DADOS_H_INCLUDED
-void dibujarCuadrado(int posX,int posY);
-void dibujarDados(int ,int ,int);
-void dibujarPuntos(int numero,int posX,int posY);
-
-
-#endif // DADOS_H_INCLUDED
